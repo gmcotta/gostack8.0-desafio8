997bef72a6e74ddd0d7e3d80d22e0070213c008d
Gustavo Cotta <gmcotta34@gmail.com>
2019-10-15 20:34:58 -0300

    Fixing reactotron config

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import { StatusBar } from 'react-native';
 import { Provider } from 'react-redux';
+import './config/ReactotronConfig';
 import Routes from './routes';
 import NavigationService from './services/navigation';
 
 import store from './store';
-import './config/ReactotronConfig';
 
 export default function App() {
   return (
